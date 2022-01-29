c446d6c8af02524eac97bab93ccbaf59bbae7bd1
Fabio Rosada <r.fabio95@gmail.com>
2022-01-30 00:03:56 +0100

    Updated setup.sh and requirements file

--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-echo -e "\n##########################\n\n"
-echo -e "Downloading the data from the professor's website...\n"
+echo -e "\n##########################\n"
+echo "Downloading the data from the professor's website..."
+echo -e "\n##########################\n"
 wget http://www.dais.unive.it/~bergamasco/teachingfiles/cvstuff2019/cv2019_data.7z
 
-echo -e "\n##########################\n\n"
-echo -e "Unzipping the data...\n"
+echo -e "\n##########################\n"
+echo "Unzipping the data..."
+echo -e "\n##########################\n"
 7z x cv2019_data.7z
 
 
-echo -e "\n##########################\n\n"
-echo -e "Removing the compressed file...\n"
+echo -e "\n##########################\n"
+echo "Removing the compressed file..."
+echo -e "\n##########################\n"
 rm cv2019_data.7z
 
-echo -e "\n##########################\n\n"
-echo "Next you should run 'conda create --name cv --file requirements.txt -c conda-forge"
-
+echo -e "\n##########################\n"
+echo "Next you should run 'conda create -n cv --file requirements.txt -c conda-forge -c open3d-admin'"
+echo -e "\n##########################\n"
